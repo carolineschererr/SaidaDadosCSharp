5e1e5e2fdf6f0f4a06a90b17497fe0c92807ec3a
Caroline Scherer <scherer.caroline@gmail.com>
2023-03-23 21:37:12 -0300

    Exercícios Saída de Dados CSharp
    Saída de dados com formatações diversas

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExercicioSaidaDados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double saldo = 50000;
+            decimal conta = 10000.8m;
+            decimal valorPago;
+            String nome, nomeAux;
+
+            Console.WriteLine("O saldo é: {0} ", saldo.ToString("F2"));
+            Console.WriteLine("A conta é: {0:00} ", conta.ToString("F2"));
+
+            Console.WriteLine("Informe o valor pago: ");
+            valorPago = Decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("O valor pago é: R$ {0}", valorPago.ToString("F2"));
+
+            Console.WriteLine("Informe o nome: ");
+            nome = Console.ReadLine();
+            nomeAux = "'" + nome + "'";
+
+            Console.WriteLine("Nome no BD: {0}", nomeAux);
+        }
+    }
+}
